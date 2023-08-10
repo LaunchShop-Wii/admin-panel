7f128c7db6c3506520995ed6317bcc9bcc53b529
notluca <lucapols51@gmail.com>
2023-08-10 12:01:48 +0200

    testing

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaunchShop Admin</title>
+    <link rel="stylesheet" href="/css/tailwind.css">
+</head>
+<body class="text-2xl">
+    launchshop test!
+</body>
+</html>
